2427375d40853ccacd0c7fb6953397802dd0b85f
Rafael Lagoinha <raftilu@gmail.com>
2016-01-14 10:02:59 +0100

    Début de la gestion des interprétations/compositions/directions

--- pages/artist/base.php
+++ pages/artist/base.php
@@ -10,9 +10,12 @@
             return;
         }
         $parameters["code"] = Template::MakeTextSafe($parameters["code"]);
-        // Récupère toutes les oeuvres et les enregistrements de l'artiste concerné
+        // Récupère toutes les interprétations de l'artiste concerné
         $sql = "SELECT DISTINCT Musicien.Code_Musicien as Code_Musicien, Composer.Code_Oeuvre as Code_Oeuvre, Oeuvre.Titre_Oeuvre as Titre_Oeuvre, Oeuvre.Sous_Titre as Sous_Titre, Oeuvre.Année as Annee, Oeuvre.Opus as Opus FROM Musicien ";
-        $sql = $sql."LEFT JOIN Composer ON Composer.Code_Musicien = Musicien.Code_Musicien LEFT JOIN Oeuvre ON Oeuvre.Code_Oeuvre = Composer.Code_Oeuvre ";
+        $sql = $sql."INNER JOIN Interpréter on Interpréter.Code_Musicien = Musicien.Code_Musicien ";
+        $sql = $sql."INNER JOIN Enregistrement on Enregistrement.Code_Morceau = Interpréter.Code_Morceau ";
+        $sql = $sql."INNER JOIN Composer on Composer.Code_Musicien = Musicien.Code_Musicien ";
+        $sql = $sql."INNER JOIN Oeuvre on Oeuvre.Code_Oeuvre = Composer.Code_Oeuvre ";
         $sql = $sql."WHERE Musicien.Code_Musicien = '".$parameters["code"]."'";
         $results = $_system_registry->getModel()->query($sql)->fetchall();
         for($i = 0; $i != count($results); $i++)
@@ -23,7 +26,7 @@
             $results[$i]["pair"] = "pair";
           $results[$i]["Annee"] = $results[$i][utf8_decode("Annee")];
         }
-        $parameters["oeuvres"] = $results;
+        $parameters["interpretations"] = $results;
         // Récupère les informations sur l'ariste
         $results = $_system_registry->getModel()->query("SELECT Musicien.Code_Musicien as Code_Musicien, Musicien.Nom_Musicien as Nom_Musicien, Musicien.Prénom_Musicien as Prénom_Musicien, Musicien.Année_Naissance as Année_Naissance, Musicien.Année_Mort as Année_Mort, Musicien.Photo as Photo FROM Musicien WHERE Musicien.Code_Musicien = '".$parameters["code"]."'")->fetch();
         // Remplacement des chaines vides par "Inconnu"
