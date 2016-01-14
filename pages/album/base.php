c73212007624edf9620582d15942652d0690e51e
Rafael Lagoinha <raftilu@gmail.com>
2016-01-14 09:39:27 +0100

    Retrait des accents, correction des champs vides

--- pages/album/base.php
+++ pages/album/base.php
@@ -10,7 +10,7 @@
         }
         $parameters["code"] = Template::MakeTextSafe($parameters["code"]);
         // Récupère toutes les enregistrement de l'album concerné
-        $sql = "SELECT DISTINCT Album.Code_Album as Code_Album, Disque.Code_Disque as Code_Disque, Morceau.Code_Morceau as Code_Morceau, Enregistrement.Titre as Titre, Enregistrement.Durée as Durée, Enregistrement.Prix as Prix, Enregistrement.Extrait as Extrait FROM Album ";
+        $sql = "SELECT DISTINCT Album.Code_Album as Code_Album, Disque.Code_Disque as Code_Disque, Morceau.Code_Morceau as Code_Morceau, Enregistrement.Titre as Titre, Enregistrement.Durée as Duree, Enregistrement.Prix as Prix, Enregistrement.Extrait as Extrait FROM Album ";
         $sql = $sql."INNER JOIN Disque ON Disque.Code_Album = Album.Code_Album INNER JOIN Composition_Disque ON Composition_Disque.Code_Disque = Disque.Code_Disque INNER JOIN Enregistrement ON Enregistrement.Code_Morceau = Composition_Disque.Code_Morceau WHERE Album.Code_Album = '".$parameters["code"]."'";
         $results = $_system_registry->getModel()->query($sql)->fetchall();
         for($i = 0; $i != count($results); $i++)
@@ -23,10 +23,10 @@
             $results[$i]["pair"] = "pair";
         }
         $parameters["records"] = $results;
-        $results = $_system_registry->getModel()->query("SELECT DISTINCT Album.Code_Album as Code_Album, Album.Titre_Album as Titre_Album, Album.Année_Album as Année_Album, Album.Pochette as Pochette, Album.ASIN as ASIN WHERE Album.Code_Album = '".$parameters["code"]."'")->fetchall();
+        $results = $_system_registry->getModel()->query("SELECT DISTINCT Album.Code_Album as Code_Album, Album.Titre_Album as Titre_Album, Album.Année_Album as Annee_Album, Album.Pochette as Pochette, Album.ASIN as ASIN WHERE Album.Code_Album = '".$parameters["code"]."'")->fetchall();
         // Récupère les informations relatives à l'album
-        $parameters["Titre_Album"] = $results["Titre_Album"];
-        $parameters["Année_Album"] = $results["Année_Album"];
+        $parameters["Titre_Album"] = $results[utf8_decode("Titre_Album")];
+        $parameters["Annee_Album"] = $results[utf8_decode("Annee_Album")];
         $parameters["Pochette"] = $results["Pochette"];
         $parameters["ASIN"] = $results["ASIN"];
         template("views/album/base.tpl", $parameters, "views/base.tpl");
--- pages/artwork/base.php
+++ pages/artwork/base.php
@@ -3,7 +3,7 @@
     $artwork = function($parameters)
     {
         global $_system_registry;
-        
+
         if(!isset($parameters["code"]) or $parameters["code"]="")
         {
             header("Location: ".$parameters["_url"]."/404");
@@ -11,7 +11,7 @@
         }
         $parameters["code"] = Template::MakeTextSafe($parameters["code"]);
         // Récupère toutes les albums dans lesquels l'oeuvre est présente
-        $sql = "SELECT DISTINCT Oeuvre.Code_Oeuvre, Composition_Oeuvre.Code_Composition, Enregistrement.Code_Morceau, Composition_Disque.Code_Disque, Album.Code_Album as Code_Album, Album.Titre_Album as Titre_Album, Album.Année_Album as Année_Album, Album.Pochette as Pochette FROM Oeuvre ";
+        $sql = "SELECT DISTINCT Oeuvre.Code_Oeuvre, Composition_Oeuvre.Code_Composition, Enregistrement.Code_Morceau, Composition_Disque.Code_Disque, Album.Code_Album as Code_Album, Album.Titre_Album as Titre_Album, Album.Année_Album as Annee_Album, Album.Pochette as Pochette FROM Oeuvre ";
         $sql = $sql."INNER JOIN Composition_Oeuvre on Composition_Oeuvre.Code_Oeuvre = Oeuvre.Code_Oeuvre INNER JOIN Composition on Composition.Code_Composition = Composition_Oeuvre.Code_Composition INNER JOIN Enregistrement on Enregistrement.Code_Composition = Enregistrement.Code_Composition ";
         $sql = $sql."INNER JOIN Composition_Disque on Composition_Disque.Code_Morceau = Enregistrement.Code_Morceau INNER JOIN Disque on Disque.Code_Disque = Composition_Disque.Code_Disque INNER JOIN Album on Album.Code_Album = Disque.Code_Album WHERE Oeuvre.Code_Oeuvre = '".$parameters["code"]."'";
         $results = $_system_registry->getModel()->query($sql)->fetchall();
@@ -25,9 +25,9 @@
         $parameters["albums"] = $results;
         // Récupère les informations sur l'oeuvre
         $results = $_system_registry->getModel()->query("SELECT DISTINCT Oeuvre.Code_Oeuvre, Oeuvre.Titre_Oeuvre as Titre_Oeuvre, Oeuvre.Sous_Titre as Sous_Titre, Oeuvre.Année_Oeuvre as Année WHERE Oeuvre.Code_Oeuvre = '".$parameters["code"]."'")->fetchall();
-        $parameters["Titre_Oeuvre"] = $results["Titre_Oeuvre"];
-        $parameters["Sous_Titre"] = $results["Sous_Titre"];
-        $parameters["Année"] = $results["Année"];
+        $parameters["Titre_Oeuvre"] = $results[utf8_decode("Titre_Oeuvre")];
+        $parameters["Sous_Titre"] = $results[utf8_decode("Sous_Titre")];
+        $parameters["Annee"] = $results["Annee"];
         template("views/artwork/base.tpl", $parameters, "views/base.tpl");
     };
     $_system_registry->registerPage("artwork", "/code", $artwork);
